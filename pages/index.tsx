230cac87d8e1af249911a59881b153cc0d1a12de
Nico Martin <mail@nico.dev>
2022-11-10 23:09:14 +0100

    added title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,15 @@ import { useIntl } from 'react-intl';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { PluginInfos, getPluginInfos } from '@common/plugin';
 import Intro from '@app/home/Intro';
-import { Button, CellBody, CellHeading, Row, THead, Table } from '@theme';
+import {
+  Button,
+  CellBody,
+  CellHeading,
+  Row,
+  TBody,
+  THead,
+  Table,
+} from '@theme';
 import styles from './Home.module.css';
 import layoutStyles from './Layout.module.css';
 
@@ -40,71 +48,73 @@ const Home: React.FC<
       <h2>{formatMessage({ id: 'home.features.title' })}</h2>
       <p>{formatMessage({ id: 'home.features.desc' })}</p>
       <Table className={styles.featuresTable}>
-        {[
-          {
-            feature: '<b>Install and update Plugins from Git repositories</b>',
-            value: '✅',
-          },
-          {
-            feature: '<b>Provider</b>',
-            value: '',
-          },
-          {
-            feature: '- GitHub',
-            value: '✅',
-          },
-          {
-            feature: '- Gitlab',
-            value: '✅',
-          },
-          {
-            feature: '- Bitbucket',
-            value: '✅',
-          },
-          {
-            feature: '<b>Webhook updates</b>',
-            value: '✅',
-          },
-          {
-            feature:
-              '<b>Integrated WordPress update process</b><br />View pending updates directly in the WordPress overview and update them individually or as a bulk update.',
-            value: '✅',
-          },
-          {
-            feature: '<b>Private Repositories</b>',
-            value: '✅',
-          },
-          {
-            feature:
-              '<b>Must Use Plugin support</b><br /><a href="https://wordpress.org/support/article/must-use-plugins/" target="_blank">https://wordpress.org/support/article/must-use-plugins/</a>',
-            value: '✅',
-          },
-          {
-            feature: '<b>Branches</b>',
-            value: 'any branch',
-          },
-          {
-            feature: '<b>Multisite</b>',
-            value: '✅',
-          },
-          {
-            feature: '<b>Install from subdirectories</b>',
-            value: '✅',
-          },
-          {
-            feature:
-              '<b>Check directory</b><br />Validates a Repository and checks wether a valid WordPress theme or plugin is found.',
-            value: '✅',
-          },
-        ].map(({ feature, value }, i) => (
-          <Row key={i}>
-            <CellBody
-              className={styles.cellFeature}
-              dangerouslySetInnerHTML={{ __html: feature }}
-            />
-            <CellBody className={styles.cellValue}>{value}</CellBody>
-          </Row>
-        ))}
+        <TBody>
+          {[
+            {
+              feature:
+                '<b>Install and update Plugins from Git repositories</b>',
+              value: '✅',
+            },
+            {
+              feature: '<b>Provider</b>',
+              value: '',
+            },
+            {
+              feature: '- GitHub',
+              value: '✅',
+            },
+            {
+              feature: '- Gitlab',
+              value: '✅',
+            },
+            {
+              feature: '- Bitbucket',
+              value: '✅',
+            },
+            {
+              feature: '<b>Webhook updates</b>',
+              value: '✅',
+            },
+            {
+              feature:
+                '<b>Integrated WordPress update process</b><br />View pending updates directly in the WordPress overview and update them individually or as a bulk update.',
+              value: '✅',
+            },
+            {
+              feature: '<b>Private Repositories</b>',
+              value: '✅',
+            },
+            {
+              feature:
+                '<b>Must Use Plugin support</b><br /><a href="https://wordpress.org/support/article/must-use-plugins/" target="_blank">https://wordpress.org/support/article/must-use-plugins/</a>',
+              value: '✅',
+            },
+            {
+              feature: '<b>Branches</b>',
+              value: 'any branch',
+            },
+            {
+              feature: '<b>Multisite</b>',
+              value: '✅',
+            },
+            {
+              feature: '<b>Install from subdirectories</b>',
+              value: '✅',
+            },
+            {
+              feature:
+                '<b>Check directory</b><br />Validates a Repository and checks wether a valid WordPress theme or plugin is found.',
+              value: '✅',
+            },
+          ].map(({ feature, value }, i) => (
+            <Row key={i}>
+              <CellBody className={styles.cellFeature}>
+                <p dangerouslySetInnerHTML={{ __html: feature }} />
+              </CellBody>
+              <CellBody className={styles.cellValue}>{value}</CellBody>
+            </Row>
+          ))}
+        </TBody>
       </Table>
       <h2>{formatMessage({ id: 'home.pricing.title' })}</h2>
       <p>
