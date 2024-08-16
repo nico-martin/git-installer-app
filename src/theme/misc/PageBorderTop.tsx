441c9bd5be0a73350456165f9176721c49970adf
Nico Martin <mail@nico.dev>
2024-08-16 06:48:34 +0200

    border changes

--- src/theme/misc/PageBorderTop.tsx
+++ src/theme/misc/PageBorderTop.tsx
@@ -8,13 +8,13 @@ const PageBorderTop: React.FC<{ className?: string }> = ({
   <svg
     className={cn(className, styles.root)}
     version="1.1"
-    viewBox="0 0 4000 180"
-    enableBackground="new 0 0 4000 180"
+    viewBox="0 0 4000 220"
+    enableBackground="new 0 0 4000 220"
     preserveAspectRatio="xMinYMin meet"
   >
-    <g transform="matrix(1.02228,0,0,0.756869,-89.127,-31.9233)">
+    <g transform="matrix(3.0631e-17,-0.500243,33.4651,2.04915e-15,-2023.72,426.297)">
       <path
-        d="M4000,42.178L4000,280L87.184,280L4000,42.178Z"
+        d="M707.872,180L412.394,180L412.394,60.472L606.55,60.472L707.872,180Z"
         fill="currentColor"
       />
     </g>
